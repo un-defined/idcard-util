a080d84c7c768e9a9e82e142df61b6abd5f9bcf9
un-defined <659168590@qq.com>
2018-03-11 23:14:15 +0800

    [A] calculate last char

--- index.js
+++ index.js
@@ -1,20 +1,78 @@
 
+// TODO: 兼容15位
 (function () {
+
+    function IdCard(card) {
+        this._provinceCode = card.substr(0, 2);     // 省份代码
+        this._cityCode = card.substr(2, 2);         // 城市代码
+        this._districtCode = card.substr(4, 2);     // 区县代码
+        this._birthCode = card.substr(6, 8);        // 出生年月日
+        this._sexCode = card.substr(14, 2);         // 所在地派出所代码
+        this._policeCode = card.substr(16, 1);      // 性别
+        this._parityCode = card.charAt(17);         // 校验码
+    }
+
+    IdCard.prototype.getLastChar = function () {
+        return this._parityCode;
+    }
 
     var _idCard = {};
 
+    var _checkFormat = function (card) {
+        // TODO: 参数校验: 17或18位数字字符串，生日校验，地址编码
+        
+        // 最后一位必须为数字或者 'X'
+        var pre17Char = card.substr(0, 17);      // 证件前17位
+        var lastChar = card.charAt(17);          // 第十八位的值
+        if (!/^(\d|X)$/.test(lastChar)) return false;
+
+        return true;
+
+    }
+
+    /**
+     * 根据前17位计算出第18位，公式：∑(ai×Wi)(mod 11)
+     * @param {String} num 17或18位数字字符串
+     * @return {String | Boolean}
+     */
+    var _getLastChar = function (num) {
+        // TODO: 校验
+        
+        var factor = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ];   //加权因子
+        var parity = [ 1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2 ];                     //校验位
+        var sum = 0;
+        var ai = 0;
+        var wi = 0;
+        var last = '';
+
+        for (var i = 0; i < 17; i++) {
+            ai = num[i];
+            wi = factor[i];
+            sum += ai * wi;
+        }
+        return last = parity[sum % 11];
+    }
+
+    // _idCard.lastNum = _getLastChar;
+
+    _idCard.isValid = function(card) {
+        if (!_checkFormat(card)) return false;
+        if (_getLastChar(card).toString() !== card.charAt(17)) return false;
+        return true;
+    }
+
     if (typeof exports !== 'undefined' || (typeof module !== 'undefined' && module.exports)) {
-        module.exports = outPut;
+        module.exports = IdCard;
     } else if (typeof define === 'function' && define.amd) {
         define('idCard', [], function () {
-            return outPut;
+            return IdCard;
         });
     } else if (typeof define === 'function' && define.cmd) {
         define(function () {
-            return outPut;
+            return IdCard;
         })
     } else {
-        root.idCard = outPut;
+        window.idCard = IdCard;
     }
 
 })();
---
+++ test.js
@@ -0,0 +1,4 @@
+var IdCard = require('./index');
+var card = '421102199105120518';
+// console.log(idCard.lastNum(card));
+console.log(new IdCard(card)); 
