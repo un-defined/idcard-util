7794946435e56b2921063552adc4cbb4d511950a
Andy <659168590@qq.com>
2018-03-14 08:56:59 +0800

    [M] config user.email

--- test.js
+++ test.js
@@ -3,8 +3,3 @@ var card = '42110219910512018X';
 var cardInfo = new IdCard(card);
 
 console.log(cardInfo);
-console.log(cardInfo.getParityCode());
-console.log(cardInfo.getSex());
-console.log(cardInfo.getAge());
-console.log(cardInfo.isValid());
-console.log(cardInfo.genRandom());
